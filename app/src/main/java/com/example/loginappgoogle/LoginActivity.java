c2ae4fbe32aaeec48411f04913ea580cb989d219
User <a.naim1806@gmail.com>
2022-11-01 13:10:51 +0800

    login

--- app/src/main/java/com/example/loginappgoogle/LoginActivity.java
+++ app/src/main/java/com/example/loginappgoogle/LoginActivity.java
@@ -3,12 +3,34 @@ package com.example.loginappgoogle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etPassword);
+        loginButton = findViewById(R.id.btnLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("username")&& password.getText().toString().equals("0987")){
+                    Toast.makeText(LoginActivity.this, "Login Successful??", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login Fail??", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
